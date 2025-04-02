b14a65e3a508e35959c2a9cab53c4f00bf89cdea
Nayara <nayaramaisa.stn@gmail.com>
2025-04-02 10:12:53 -0300

    ediçoes realizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import GlobalStyle from './global'
 
 function App() {
+  console.log('App renderizado')
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
